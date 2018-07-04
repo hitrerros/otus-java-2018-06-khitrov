3e9d5be0a2772106dc2840dc2d3aeb90dae2ebe9
hitrerros <antkhit83@gmail.com>
2018-07-04 22:22:00 +0300

    lesson 1

--- src/main/java/ru/otus/khitrov2018_06/Main.java
+++ src/main/java/ru/otus/khitrov2018_06/Main.java
@@ -1,4 +1,22 @@
 package ru.otus.khitrov2018_06;
 
+import org.apache.commons.text.*;
+
+
+
+
 public class Main {
+
+    public static void main(String[] args){
+
+        TextStringBuilder textStringBuilder = new TextStringBuilder("Hello world!");
+        StringTokenizer stringTokenizer =  textStringBuilder.reverse().asTokenizer();
+
+
+        while (stringTokenizer.hasNext()) {
+            System.out.println( stringTokenizer.next() );
+        }
+
+    }
+
 }
