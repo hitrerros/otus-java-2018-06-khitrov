61199f2de5585c618f8c74de1211cebe10e3746d
hitrerros <antkhit83@gmail.com>
2018-10-10 12:24:07 +0300

    HW10 MyORM

--- HW10_1_JDBC/src/main/java/ru/otus/khitrov/myorm/LogExecutorORM.java
+++ HW10_1_JDBC/src/main/java/ru/otus/khitrov/myorm/LogExecutorORM.java
@@ -9,13 +9,14 @@ public class LogExecutorORM extends  LogExecutor {
 
    public LogExecutorORM(Connection connection) throws SQLException {
         super(connection);
-        connection.setAutoCommit(true);
+        connection.setAutoCommit(false);
     }
 
 
     public <T> T execUpdate(String query, TResultHandler<T> handler) throws Exception {
         try (Statement stmt = connection.createStatement()) {
             stmt.execute(query, Statement.RETURN_GENERATED_KEYS);
+            connection.commit();
             ResultSet result = stmt.getGeneratedKeys();
             return handler.handle(result);
         }
@@ -25,7 +26,6 @@ public class LogExecutorORM extends  LogExecutor {
 
         try (Statement stmt = connection.createStatement()) {
             for (String query: queries)  stmt.addBatch(query);
-
             stmt.executeBatch();
       }
     }
